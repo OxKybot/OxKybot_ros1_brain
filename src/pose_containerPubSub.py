1c56c0f524a86ea9dc63c60247a7727646f67bec
OxKybot <103052308+OxKybot@users.noreply.github.com>
2022-06-14 21:43:33 +0200

    Update pose_containerPubSub.py

--- src/pose_containerPubSub.py
+++ src/pose_containerPubSub.py
@@ -2,28 +2,26 @@
 import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import PoseStamped
-from SlamPose import SlamPose
 
-actualPose = SlamPose()
+
+actualPose = PoseStamped()
 def publishMsg(publisher,msg,log_text):
     rospy.loginfo(log_text)
     publisher.publish(msg)
 
 def update_pose_cb(data):
-    actualPose.fromHectorToOxKybot(data)
+    global actualPose
+    actualPose = data
 
 def get_pose(data):
-    pose = str(actualPose.poseX)
-    pose += ","+str(actualPose.poseY)
-    pose += ","+str(actualPose.angle)
-    publishMsg(poseToArduinoPub,pose,"pose requested")
+    publishMsg(poseToArduinoPub,actualPose,"pose requested")
 
 def start():
 
     rospy.Subscriber("slam_out_pose", PoseStamped, update_pose_cb)
     rospy.Subscriber("POSITION_node", String, get_pose)
     global poseToArduinoPub
-    poseToArduinoPub = rospy.Publisher('get_position_response', String, queue_size=10)
+    poseToArduinoPub = rospy.Publisher('get_position_response', PoseStamped, queue_size=10)
     global poseNode
     poseNode = rospy.Publisher('position', String, queue_size=10)
     rospy.init_node('poseContainer', anonymous=True)
